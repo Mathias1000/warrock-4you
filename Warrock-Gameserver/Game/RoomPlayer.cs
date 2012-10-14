9a670ff88a5bbbe2dc424a8567129daac3ae87af
Mathias-100@hotmail.de <Mathias-100@hotmail.de@3bc999aa-9e12-677e-6f1b-12dea7674fe1>
2012-10-14 20:35:17 +0000

    Add missing Class

---
+++ Warrock-Gameserver/Game/RoomPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Warrock.Networking;
+
+namespace Warrock.Game
+{
+    public class RoomPlayer
+    {
+        private GameClient pClient { get; set; }
+
+        public RoomPlayer(GameClient Client)
+        {
+            this.pClient = Client;
+        }
+    }
+}
---
+++ Warrock-Gameserver/Game/item/Item.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Warrock.Database;
+
+namespace Warrock.Game.Item
+{
+    public class item
+    {
+        public string itemCode { get; set; }
+        public long expireDate { get; set; }
+        public byte Class { get; set; }
+        public byte BandageSlot { get; set; }
+
+        public static item LoadFromDatabase(DataRow Row)
+        {
+            item pItem = new item
+            {
+                itemCode = Row["ItemCode"].ToString(),
+                expireDate = GetDataTypes.GetLong(Row["expireDate"]),
+                Class = GetDataTypes.GetByte(Row["Class"]),
+                BandageSlot = GetDataTypes.GetByte(Row["BandageSlot"]),
+            };
+            return pItem;
+        }
+    }
+}
