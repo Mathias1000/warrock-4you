501da4cd82269df75d6be6183e41b3f478a5ca7c
Mathias-100@hotmail.de <Mathias-100@hotmail.de@3bc999aa-9e12-677e-6f1b-12dea7674fe1>
2012-10-15 10:21:11 +0000

    Add CommandHandlers and Add Mutex in Inventory and add Add and Remove Function for items

---
+++ Warrock-Gameserver/CommandHandlers/CommandInfo/IngameCommandInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Warrock.CommandHandlers
+{
+    public class IngameCommandInfo
+    {
+        public string Command { get; private set; }
+        public IngameCommandHandler.Command Function { get; private set; }
+        public string[] Parameters { get; private set; }
+        public byte GmLevel { get; private set; }
+
+        public IngameCommandInfo(string command, IngameCommandHandler.Command function, byte gmlevel, string[] param)
+        {
+            Command = command;
+            Function = function;
+            GmLevel = gmlevel;
+            Parameters = param;
+        }
+    }
+}
