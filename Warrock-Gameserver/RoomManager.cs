7563d9bf058881dba7bc9b56b51f668a4037dfe4
Mathias-100@hotmail.de <Mathias-100@hotmail.de@3bc999aa-9e12-677e-6f1b-12dea7674fe1>
2012-10-16 08:56:24 +0000

    Add RoomManager :D

---
+++ Warrock-Gameserver/RoomManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Warrock.Util;
+using System.Collections.Concurrent;
+using Warrock.Game;
+
+namespace Warrock
+{
+    [ServerModule(Util.InitializationStage.DataStore)]
+    public class RoomManager
+    {
+        public static RoomManager Instance { get; private set; }
+        public  ConcurrentDictionary<int, PlayerRoom> ServerRooms = new ConcurrentDictionary<int, PlayerRoom>();
+        public int MaxRoomPages = 17; //17 = 254 rooms
+
+        [InitializerMethod]
+        public static bool Load()
+        {
+            
+            Instance = new RoomManager()
+            {
+            };
+            Log.WriteLine(LogLevel.Info, "RoomManager initialized.");
+            return true;
+        }
+
+        public bool GetEmptyRoomSlot(out int pSlot)
+        {
+            pSlot = 0;
+            IEnumerable<int> keyRange = Enumerable.Range(0, 15 * MaxRoomPages);
+            var freeKeys = keyRange.Except(ServerRooms.Keys);
+            if (freeKeys.Count() == 0)
+                return false; // no free slot
+
+            pSlot = freeKeys.First();
+            return true;
+        }
+    }
+}
